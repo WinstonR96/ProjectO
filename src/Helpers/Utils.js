ad3b6674bd3d5f17dd5f1d6fbe8e8296f909d3af
Winston Jr Rodriguez Stand <wijurost@gmail.com>
2019-11-15 11:49:31 -0500

    Creando el helpers Utils

---
+++ src/Helpers/Utils.js
@@ -0,0 +1,32 @@
+import Swal from "sweetalert2";
+
+const Utils = {
+  AlertaUsuarioNoEncontrado: () => {
+    Swal.fire({
+      title: "No encontrado",
+      text:
+        "Tu número no esta registrado, por favor acércate al módulo de Atencion al Cliente",
+      confirmButtonText: "Volver",
+      confirmButtonColor: "#B4AFAE"
+    });
+  },
+  AlertaDatosIncompletos: () => {
+    Swal.fire({
+      title: "Información incorrecta",
+      text: "Digite Campo vacio",
+      icon: "error",
+      confirmButtonText: "Ok"
+    });
+  },
+  AlertaOcurrioUnError: message => {
+    Swal.fire({
+      title: "Ocurrio un error",
+      text: message,
+      icon: "error",
+      confirmButtonText: "Volver",
+      confirmButtonColor: "#d33"
+    });
+  }
+};
+
+export default Utils;
