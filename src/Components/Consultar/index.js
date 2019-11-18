4396ec62bb93adfc0c15e4a8b59581f9cd673c28
Winston Jr Rodriguez Stand <wijurost@gmail.com>
2019-11-18 16:30:56 -0500

    quitar los simbolos y shift del teclado

--- src/Components/Consultar/index.js
+++ src/Components/Consultar/index.js
@@ -134,8 +134,8 @@ class Consultar extends Component {
                 onKeyPress={button => this.onKeyPress(button)}
                 layoutName={this.state.layoutName}
                 layout={{
-                  default: ["1 2 3", "4 5 6", "7 8 9", "{shift} 0 _", "{bksp}"],
-                  shift: ["! / #", "$ % ^", "& * (", "{shift} ) +", "{bksp}"]
+                  default: ["1 2 3", "4 5 6", "7 8 9", " 0 ", "{bksp}"],
+                  shift: ["! / #", "$ % ^", "& * (", " ) +", "{bksp}"]
                 }}
                 display={{
                   "{lock}": "mayus",
