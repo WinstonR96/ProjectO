f564218b86bb35b5c7184689fd7dc7eb8b0d67ea
Winston Jr Rodriguez Stand <wijurost@gmail.com>
2019-11-19 16:26:09 -0500

    Desarrollando visual del componente firma

--- src/Components/Firma/index.js
+++ src/Components/Firma/index.js
@@ -6,10 +6,39 @@ import Header from "./../Global/Header";
 import{ FaTrash } from "react-icons/fa";
 
 class Firma extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      firma: "",
+      estaFirmando: false,
+      clase: ""
+    };
+  }
 
   componentDidMount = () => {
-    this.signaturePad.clear()
-    this.signaturePad.Background = null;
+    this.setState({
+      firma: "",
+      clase: "firma"
+    });
+    this.signaturePad.clear();
+  }
+
+
+  //Manejo de la firma
+  handleFirma = () => {
+    this.setState({
+      estaFirmando: true,
+      clase: ""
+    });
+  }
+
+  borrarFirma = () => {
+    this.signaturePad.clear();
+    this.setState({
+      estaFirmando: false,
+      clase: "firma"
+    });
   }
 
   render(){
@@ -19,14 +48,21 @@ class Firma extends Component {
         <h3 className={"title"}>Actualización de datos personales</h3>
         <p className={"leyenda-firma"}>Firma en el recuadro para terminar el proceso de actualización</p>
         <div className={"contenedor-firma"}>
-          <FaTrash />{" "}
-          <SignaturePad
-            ref={ref => this.signaturePad = ref}
-            width={800}
-            height={400}
-            penColor={"#000"}
-          />
+          {this.state.estaFirmando ? <div className={"trash-firma"}><span className={"icon-trash"} onClick={this.borrarFirma} ><FaTrash />{" "} Borrar todo</span></div> : <div className={"trash-firma"}></div> }
+            <div className={this.state.clase} onClick={this.handleFirma}>
+              <SignaturePad
+                ref={ref => this.signaturePad = ref}
+                width={800}
+                height={400}
+                penColor={"#000"}
+              />
+            </div>
         </div>
+        <p className={"leyenda-firma"}>Al presionar aceptar, estás aceptando nuestra Política de manejo de datos personales</p>
+        <FormGroup className={"form-button-firma"}>
+         {this.state.estaFirmando ? <Button color={"success"}>Aceptar</Button> : <Button disabled>Aceptar</Button> }
+          <Button>Cancelar</Button>
+        </FormGroup>
       </div>
     );
   }
